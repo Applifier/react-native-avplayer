236fac593547567dee49b53bd0ae2683f1a41f8f
Üstün Ergenoglu <ustun@unity3d.com>
2016-08-29 15:27:08 +0300

    Remove video flipping hack

--- android/src/main/java/com/unity3d/rctavplayer/RCTAVPlayerLayer.java
+++ android/src/main/java/com/unity3d/rctavplayer/RCTAVPlayerLayer.java
@@ -87,12 +87,6 @@ public class RCTAVPlayerLayer extends ScalableVideoView implements RCTAVPlayer.L
                 public void run()
                 {
                     setScalableType(resizeMode);
-                    //HACK: For some reason, need to flip the videos vertically here.
-                    Matrix m = new Matrix();
-                    getTransform(m);
-                    m.postScale(1.0f, -1.0f, 0.0f, getHeight());
-                    setTransform(m);
-                    invalidate();
                 }
             });
         }
